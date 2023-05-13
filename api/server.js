0896571dd9b80976ad6ee7c8f4c47211bfe5d29e
inza01-git <facundoinza5@gmail.com>
2023-05-13 15:28:27 -0300

    Conexion de base de datos con server

--- api/server.js
+++ api/server.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const config = require("./config");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const db = require("./db");
+
+//CONSTANTES
+const PORT = config.PORT;
 const app = express();
 
-const port = 3001;
+//MIDLEWARES
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT"],
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("tiny"));
 
-app.listen(port, () => {
-  console.log("Servidor escuchando en ", port);
+db.sync({ force: false }).then(() => {
+  console.log("db connected");
+  app.listen(PORT, () => {
+    console.log(`El servidor esta escuchando en el puerto ${PORT}`);
+  });
 });
