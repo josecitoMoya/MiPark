d15ad6d8778c63368f3d3e23e66f90afc9fb5c09
inza01-git <facundoinza5@gmail.com>
2023-05-18 21:51:08 -0300

    fix module exports hours

--- api/models/index.js
+++ api/models/index.js
@@ -1,11 +1,9 @@
 const Users = require("./Users.js");
 const Parkings = require("./Parkings.js");
 const Reserves = require("./Reserves");
-const Hours = require("./Hours.js");
 
 Parkings.belongsTo(Users, { as: "owner" });
 Reserves.belongsTo(Users, { as: "client" });
 Reserves.belongsTo(Parkings, { as: "parking" });
-Hours.belongsTo(Parkings, { as: "parking" });
 
-module.exports = { Parkings, Users, Reserves, Hours };
+module.exports = { Parkings, Users, Reserves };
