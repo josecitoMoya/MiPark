36945ad07a37e2f75d80ddce6464b43c9b567599
bensznaider <bensznaider@yahoo.com.ar>
2023-05-21 00:00:21 -0300

    Rutas/controlador de admin para users agregado

--- api/routes/parkings.js
+++ api/routes/parkings.js
@@ -4,11 +4,8 @@ const router = express.Router();
 const ParkingController = require("../controllers/parkings");
 
 router.get("/search/allparkings", ParkingController.getAllParkings);
-router.get("/search/allpendingparkings", ParkingController.getPendingParkings);
 router.get("/search/:id", ParkingController.getOneParking);
 router.get("/search/", ParkingController.getParkingsByCategories);
 router.post("/createparking", ParkingController.createParking);
-router.put("/authorizeparking", ParkingController.authorizeParking);
-router.delete("/deleteparking", ParkingController.deleteParking);
 
 module.exports = router;
